fd5e99df4d66432162e00d1d51544ecfd4b18d69
daniyal <dd275507@gmail.com>
2025-08-16 01:23:42 -0700

    🔁 Redeploy: Updated env or code

--- routes/products.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getProducts, getProductById } = require("../controllers/productController");
-
-// Public routes
-router.get("/", getProducts);
-router.get("/:id", getProductById);
-
-module.exports = router;
